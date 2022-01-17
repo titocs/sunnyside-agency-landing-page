5ea65a29b064561aecbaa7d27c6f316c73f2636d
Hfn <55591161+titocs@users.noreply.github.com>
2022-01-17 18:02:07 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,17 +3,12 @@ const arrow = document.querySelector(".arrow");
 const hambuger = document.querySelector("#image-hamburger");
 const contact = document.querySelector("#contact");
 const triangle = document.querySelector(".triangle");
+const topNavLi = document.querySelector(".top-nav li");
 
 hambuger.addEventListener("click", function(){
     topNav.classList.toggle("displayBlock");
     topNav.classList.toggle("displayTopNav");
-    contact.style.color = "var(--Very-dark-desaturated-blue)";
-    contact.style.fontFamily = "'Fraunces', serif";
-    contact.style.backgroundColor = "var(--Yellow)";
-    contact.style.borderRadius = "25px";
-    contact.style.padding = "15px 21px";
-    contact.style.textDecoration = "none";
-    contact.style.marginTop = "1em";
     hambuger.classList.toggle("setOpacity");
     triangle.classList.toggle("displayBlock");
 });
+
